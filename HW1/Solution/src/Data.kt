7b57035b27bbc8740aa37b5cf4eeaf3fce54d87e
mahir <mahir.suljic@fet.ba>
2025-11-03 00:07:09 +0100

    Added solution files

---
+++ HW1/Solution/src/Data.kt
@@ -0,0 +1,98 @@
+// Data taken from Google Play Store
+val appList = listOf<Application>(
+    Application(
+        "ChatGPT",
+        Category.Productivity,
+        500_000_000,
+        4.6,
+        23.08
+    ),
+
+    Application(
+        "Temu",
+        Category.Shopping,
+        500_000_000,
+        4.5,
+        67.0
+    ),
+
+    Application(
+        "WhatsApp",
+        Category.Communication,
+        10_000_000_000,
+        4.3,
+        48.02
+    ),
+
+    Application(
+        "Samsung Smart Switch Mobile",
+        Category.Tools,
+        1_000_000_000,
+        4.2,
+        81.53
+    ),
+
+    Application(
+        "Instagram",
+        Category.Social,
+        5_000_000_000,
+        4.3,
+        99.26
+    ),
+
+    Application(
+        "Microsoft Teams",
+        Category.Business,
+        500_000_000,
+        4.6,
+        61.74
+    ),
+
+    Application(
+        "Duolingo",
+        Category.Education,
+        500_000_000,
+        4.6,
+        108.0
+    ),
+
+    Application(
+        "Raiffeisen Mobile Banking",
+        Category.Finance,
+        100_000,
+        4.7,
+        183.0
+    ),
+
+    Application(
+        "Netflix",
+        Category.Entertainment,
+        1_000_000_000,
+        3.8,
+        4.32
+    ),
+
+    Application(
+        "8 Ball Pool",
+        Category.Games,
+        1_000_000_000,
+        4.8,
+        104.0
+    ),
+
+    Application(
+        "LifestyleApp1",
+        Category.Lifestyle,
+        100_000,
+        2.0,
+        100.0
+    ),
+
+    Application(
+        "LifestyleApp2",
+        Category.Lifestyle,
+        200_000,
+        4.0,
+        300.0
+    )
+)
---
+++ HW1/Solution/src/Main.kt
@@ -0,0 +1,81 @@
+fun main() {
+    filterTests()
+    groupTests()
+    findTests()
+    constructorTests()
+
+    println("\nAll tests passed!")
+}
+
+fun filterTests() {
+    val limit = 4.5
+    val expected = 6
+
+    check(customFilterByRatingAbove(appList, limit).count() == expected)
+    check(filterBy(appList, { app -> app.rating >= limit }).count() == expected)
+
+    println("Filter tests passed.")
+}
+
+fun groupTests() {
+    val expected = 11
+
+    check(customGroupByCategory(appList).count() == expected)
+    check(groupByCategory(appList).count() == expected)
+
+    println("Group tests passed.")
+}
+
+fun findTests() {
+    check(findByName(appList, "Netflix") != null)
+    check(findByName(appList, "netflix") != null)
+    check(findByName(appList, "Netfli") == null)
+
+    println("Find tests passed.")
+}
+
+fun constructorTests() {
+    val valid = Application("App", Category.Education, 1_000_000, 5.0, 100.0)
+    var thrown = false
+
+    // name test
+    try {
+        Application("", valid.category, valid.downloads, valid.rating, valid.size)
+    } catch (_: Exception) {
+        thrown = true
+    }
+
+    check(thrown)
+    thrown = false
+
+    // downloads test
+    try {
+        Application(valid.name, valid.category, -1, valid.rating, valid.size)
+    } catch (_: Exception) {
+        thrown = true
+    }
+
+    check(thrown)
+    thrown = false
+
+    // rating test
+    try {
+        Application(valid.name, valid.category, valid.downloads, 0.0, valid.size)
+    } catch (_: Exception) {
+        thrown = true
+    }
+
+    check(thrown)
+    thrown = false
+
+    // size test
+    try {
+        Application(valid.name, valid.category, valid.downloads, valid.rating, 0.0)
+    } catch (_: Exception) {
+        thrown = true
+    }
+
+    check(thrown)
+
+    println("Constructor tests passed.")
+}
